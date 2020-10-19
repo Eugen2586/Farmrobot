14d6dfe44220d235b4de0b1d1448a72e14c1c8cf
ChrisK <Christian.Kuellmer@netdate-lab.de>
2020-10-19 21:22:48 +0200

    Update Log no Compile :). :)

--- Farmrobo/src/main/java/Functions/Log.java
+++ Farmrobo/src/main/java/Functions/Log.java
@@ -2,8 +2,10 @@ package Functions;
 import org.json.simple.JSONObject;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Log {
 
@@ -14,21 +16,41 @@ public class Log {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        JSONObject obj = new JSONObject();
+        obj.put("ID",String.valueOf(k.getID()));
+        obj.put("X",String.valueOf(k.getX()) );
+        obj.put("Y",String.valueOf(k.getY()) );
+        obj.put("Z",String.valueOf(k.getZ()) );
+        obj.put("T",String.valueOf(k.getT()) );
+        obj.put("V",String.valueOf(k.getV()) );
         try {
             FileWriter myWriter = new FileWriter(k.getID() + ".txt", true);
-            JSONObject obj = new JSONObject();
-            obj.put("ID",String.valueOf(k.getID()));
-            obj.put("X",String.valueOf(k.getX()) );
-            obj.put("Y",String.valueOf(k.getY()) );
-            obj.put("Z",String.valueOf(k.getZ()) );
-            obj.put("T",String.valueOf(k.getT()) );
-            obj.put("V",String.valueOf(k.getV()) );
             myWriter.write(obj.toString());
-
             myWriter.close();
-            System.out.println("Successfully wrote to the file.");
         }catch(Exception e){
             e.printStackTrace();
         }
+        try{
+            File myObj = new File("0.txt");
+            myObj.createNewFile();
+        }catch(Exception e){
+
+        }
+    }
+
+
+    ArrayList<Koodinates> getLog(){
+        ArrayList<Koodinates> kod = new ArrayList<Koodinates>();
+        File f = new File("0.txt");
+        File[] fileArray = f.listFiles();
+        for (Object obj : fileArray ) {
+            try{
+
+            }catch(Exception e){
+
+            }
+        }
+
+        return kod;
     }
 }
