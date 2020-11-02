356178678f5eb67e56f1e0e5160b0e823109fdd6
ChrisK <Christian.Kuellmer@netdate-lab.de>
2020-11-02 23:53:01 +0100

    Temperatursensor hinzugefügt

--- X_MotorTreiber/Motortreiber_X/Motortreiber_X.ino
+++ X_MotorTreiber/Motortreiber_X/Motortreiber_X.ino
@@ -1,26 +1,40 @@
-#include <ESP8266WiFi.h>
-#include <ArduinoJson.h>
+#include <ESP8266WiFi.h>  // Für die Wifi Anbindung und den Socket Port
+#include <ArduinoJson.h>  // Für die JSON Verarbeitung
+#include <OneWire.h>      //OneWire Anbindung des TempSonsors
+#include <DallasTemperature.h> // Temperatursensorbibeothek
+
 
 // Constant Zone
-const char* ssid = "Chr.Network";
-const char* password = "2570419532734084";
-StaticJsonDocument<200> doc;
- 
-WiFiServer wifiServer(9012);
+  const char* ssid = "Chr.Network";
+  const char* password = "2570419532734084";
+  WiFiServer wifiServer(9012);
+  StaticJsonDocument<200> doc;
+//Temperatursensor
+  const int oneWireBus = D3;
+  OneWire oneWire(oneWireBus);
+  DallasTemperature sensors(&oneWire);
  
 void setup() {
+
+  //Sensoren
+  //Wassersensor
+  pinMode(A0, INPUT);//WasserSensorPin
+
+  //Temperatursensor
+  sensors.begin();
+  
+  //MotorStuff
   pinMode(D0, OUTPUT); //Enable
   pinMode(D1, OUTPUT); //Direction
   pinMode(D2, OUTPUT); //Pulse
-  pinMode(A0, INPUT);
+
 
   digitalWrite(D0,LOW); 
   digitalWrite(D1,LOW);
   
     
-  //Com Serial
-  Serial.begin(115200);
-  Serial.print("Motor initialisiert!"); 
+  //CommunicationsStuff Wifi
+  Serial.begin(115200); 
   delay(1000);
   //Wifi and Communication
   WiFi.begin(ssid, password);
@@ -31,7 +45,7 @@ void setup() {
   Serial.print("Connected to WiFi. IP:");
   Serial.println(WiFi.localIP());
   wifiServer.begin();
-  //MotorStuff
+  
 
 }
  
@@ -46,15 +60,24 @@ void loop() {
       while (client.available()>0) {
         char c = client.read();
         if(c == 'W'){
-          //Hier zum Beispiel
+          //Wassersensor
           doc["T"] = "Wassersensor";
           doc["V"] = analogRead(A0);
           char message[200];
           serializeJson(doc, message);
           client.println(message);
           }
-        else if(c == 'A'){
-          Serial.write("Ich will deinen Scheiß befehl nicht ausführen");
+        else if(c == 'T'){
+          //Temperatursensor
+           sensors.requestTemperatures(); 
+           float temperatureC = sensors.getTempCByIndex(0);
+           doc["T"] = "Temperatursensor";
+           doc["V"] = temperatureC;
+           char message[200];
+           serializeJson(doc, message);
+           client.println(message);
+        }else if(c == 'A'){
+           Serial.write("Ich will deinen Scheiß befehl nicht ausführen");
         }else if(c = 'L'){
           for(int index = 0; index < 10000; index++){
             digitalWrite(D2,HIGH);
