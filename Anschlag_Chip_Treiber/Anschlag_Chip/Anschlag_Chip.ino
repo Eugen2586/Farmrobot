93f60b58922ee93d86b35d7bbef91b053dc448fc
JoBros <JoBros@users.noreply.github.com>
2022-01-13 12:17:00 +0100

    Update Anschlag_Chip.ino

--- Anschlag_Chip_Treiber/Anschlag_Chip/Anschlag_Chip.ino
+++ Anschlag_Chip_Treiber/Anschlag_Chip/Anschlag_Chip.ino
@@ -1,23 +1,14 @@
 #include <WiFi.h>  //ESP Type
 #include <ArduinoJson.h>  
 
-//
+boolean debug = false;
 boolean A = false;
 boolean D = false;
 
-ICACHE_RAM_ATTR void higha(){
-  A = true;
-}
-ICACHE_RAM_ATTR void lowa(){
-  A = false;
-}
-
-ICACHE_RAM_ATTR void highd(){
-  D = true;
-}
-ICACHE_RAM_ATTR void lowd(){
-  D = false;
-}
+ICACHE_RAM_ATTR void higha(){A = true;}
+ICACHE_RAM_ATTR void lowa(){A = false;}
+ICACHE_RAM_ATTR void highd(){D = true;}
+ICACHE_RAM_ATTR void lowd(){D = false;}
 
 // Constant Zone
 //-> Client für die Signalübertragung
@@ -36,10 +27,89 @@ WiFiServer wifiServer(9012);
 const int SensorA = 34;
 const int SensorD = 35;
 
+
 void setup() {
+  if(debug){
   Serial.begin(115200);
+  Serial.println("Setup :)");
+  }
   initPins();
-  
+  beginWifi();
+}
+
+void loop() {
+  WiFiClient client = wifiServer.available();
+  if (client) {
+    if(debug){
+    Serial.println("Client zugeschaltet");
+    }
+    while (client.connected()) {
+      Serial.write(client.available());
+      yield();
+      while (client.available()>0) {
+        char c = client.read();
+        if(debug){
+        Serial.print("Anfrage: ");
+        Serial.println(c);
+        }
+        //Fallunterscheidung der Richtungsfreigabeanfrage
+        switch ( c ){
+          case 'G': //Grundpositiom
+          case 'A': //nach links fahren
+             if(digitalRead(SensorA) == HIGH){
+                A = true;
+             }else{
+                A = false;
+             };
+            if(A == false){
+              client.println("n,");
+              if(debug){
+              Serial.println("Antwort: n,");
+              }
+              client.stop();
+            }else{
+              client.println("y,");
+              if(debug){
+              Serial.println("Antwort: y,");
+              }
+              client.stop();
+            }
+            break;
+          case 'D': //nach rechts fahren
+            if(D == false){
+              client.println("n,");
+              if(debug){
+              Serial.println("Antwort: n,");
+              }
+              client.stop();
+            }else{
+              client.println("y,");
+              if(debug){
+              Serial.println("Antwort: y,");
+              }
+              client.stop();
+            }
+            break;
+          default:
+            Serial.write(c);
+            client.stop();
+            break;
+          }
+      }
+    }
+    client.stop();
+    if(debug){
+    Serial.println("Client disconnected");
+    }
+  }
+}
+
+void initPins(){
+  pinMode(SensorA, INPUT); //Enable
+  pinMode(SensorD, INPUT); //Direction
+  A = false;
+  D = false;
+
   //Wenn wir High bekommen, haben wir den Sensor nicht erreicht
   // ein High führt zur Freigabe y, ein low führt zum Sperren n
   if(digitalRead(SensorA) == HIGH){
@@ -52,80 +122,26 @@ void setup() {
   }else{
       D = false;
   };
-  //Nachdem wir einmal die Sensoren initialisiert haben, reagieren wir jetzt auf Änderungen.
-  
+
+  //Interruptidee:
+  //Nachdem wir einmal die Sensoren initialisiert haben, reagieren wir jetzt auf Änderungen:
   //attachInterrupt(digitalPinToInterrupt(SensorA), higha, HIGH);
   //attachInterrupt(digitalPinToInterrupt(SensorA), lowa, LOW);
   //attachInterrupt(digitalPinToInterrupt(SensorD), highd, HIGH);
   //attachInterrupt(digitalPinToInterrupt(SensorD), lowd, LOW);
-  
+}
+
+void beginWifi(){
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
+    if(debug){
     Serial.println("Connecting..");
+    }
   }
+  if(debug){
   Serial.print("Connected to WiFi. IP:");
   Serial.println(WiFi.localIP());
-  wifiServer.begin();
-}
-
-void initPins(){
-  pinMode(SensorA, INPUT); //Enable
-  pinMode(SensorD, INPUT); //Direction
-  A = false;
-  D = false;
-}
-
-void loop() {
- 
-  WiFiClient client = wifiServer.available();
-  if (client) {
-    Serial.println("Client zugeschaltet");
-    while (client.connected()) {
-      Serial.write(client.available());
-      yield();
-      while (client.available()>0) {
-        char c = client.read();
-        Serial.print("Anfrage: ");
-        Serial.println(c);
-        switch ( c ){
-          case 'G':
-          case 'A':
-             if(digitalRead(SensorA) == HIGH){
-                A = true;
-             }else{
-                A = false;
-             };
-            if(A == false){
-              client.println("n,");
-              Serial.println("Antwort: n,");
-              client.stop();
-            }else{
-              client.println("y,");
-              Serial.println("Antwort: y,");
-              client.stop();
-            }
-            break;
-          case 'D':
-            if(D == false){
-              client.println("n,");
-              Serial.println("Antwort: n,");
-              client.stop();
-            }else{
-              client.println("y,");
-              Serial.println("Antwort: y,");
-              client.stop();
-            }
-            break;
-          default:
-            Serial.write(c);
-            client.stop();
-            break;
-          }
-      }
-      delay(100);
-    }
-    client.stop();
-    Serial.println("Client disconnected");
   }
+  wifiServer.begin();
 }
